d19fd3e046ed61530fcc23a55fcaa2a6dd2cb273
Ebenezer Akinsanya <52239629+Akinebee@users.noreply.github.com>
2026-04-01 13:23:35 +0100

    Springboot
    SpringBoot

---
+++ src/main/java/com/monieclick/monieclick/MonieclickApplication.java
@@ -0,0 +1,13 @@
+package com.monieclick.monieclick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonieclickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonieclickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monieclick/monieclick/MonieclickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monieclick.monieclick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonieclickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
